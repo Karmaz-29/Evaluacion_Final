26476868b54599c1f6cc466c346bb2a6d1164011
Karol Castro <karolcastroor0305@gmail.com>
2022-05-03 09:42:26 -0600

    Animal favorito
    Archivo que pregunta el animal favorito en relaci'on al m'io.

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ print('Karol Mariann Castro Ordoñez')
 print('Carne 1545522')
 print('Estudiante del surso de introducción a la ingenieria en informatica y sistemas')
 print('Seccion 01')
+
+print('Por favor ingrese su animal favorito: ')
+animal = input()
+if animal == 'gato':
+    print('A mí también me gustan los gatos')
+else:
+    print('Es un animal interesante, pero me gustan más los gatos')
